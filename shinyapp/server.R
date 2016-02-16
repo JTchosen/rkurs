9d4654e269a7d4f15c2da0f8dc6b97f2871dc7b2
Jan <janthechosen@yahoo.de>
2016-02-16 03:16:47 +0100

    Graphen basierend auf Laenge, Jahr und Genre funktionstuechtig

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -1,35 +1,88 @@
 library(shiny)
 source("funcs.R")
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
   data <- read.table("movies.tab", sep="\t", header=TRUE, quote="", comment="")
   genres <- c("Action", "Animation", "Comedy", "Drama", "Documentary", "Romance", "Short")
   data <- factorfunc(data, genres)
-  #subset <- queryfunc(data, "Action", {input$rangeSlider[1]}, {input$rangeSlider[2]})
-  
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
-  
-   output$ImdbPlot <- renderPlot({
-     #data <- queryfunc(data, input$Genre, {input$JahrRange[1]}, {input$JahrRange[2]})
-     data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
-     plot(data$year, data$length)
+  #Generische Funktion um den Datensatz entsprechend der im UI 
+  #festgelegten Kriterien zu filtern  
+  queryfunc <- function(data, genre, anfangsjahr, endjahr, mindauer, maxdauer) {
+    tmp <- subset(data, anfangsjahr<=data$year&data$year<=endjahr&data[[genre]]==1&
+                    mindauer<=data$length&data$length<=maxdauer)
+    return(tmp)
+  }
+
+  #Speichert in der Output Variable ImdbJahrLaenge einen Plot 
+  #mit X-Achse Jahr und Y-Achse Filmlaenge.
+  #Vorher wird der Datensatz aber anhand der im UI festgelegten Kriterien gefiltert
+  #Shiny aktualisiert den Datensatz bei jeder Aenderung der Filterkriterien.
+   output$ImdbJahrLaenge <- renderPlot({
+     #data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
+     varjl <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                        input$LengthRange[1], input$LengthRange[2])
+     plot(varjl$year, varjl$length)
+   })
+   
+   output$ImdbJahrRating <- renderPlot({
+     #data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
+     varjr <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                        input$LengthRange[1], input$LengthRange[2])
+     plot(varjr$year, varjr$rating)
+   })
+   
+   output$ImdbJahrBudget <- renderPlot({
+     #data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
+     varjr <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                        input$LengthRange[1], input$LengthRange[2])
+     varjr <- subset(varjr, !is.na(varjr$budget))
+     plot(varjr$year, varjr$budget)
+   })
+   
+   output$ImdbBudgetRating <- renderPlot({
+     #data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
+     varbr <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                        input$LengthRange[1], input$LengthRange[2])
+     plot(varbr$budget, varbr$rating)
+   })
+   
+   output$ImdbLengthRating <- renderPlot({
+     #data <- subset(data, {input$JahrRange[1]}<=data$year&data$year<={input$JahrRange[2]}&data[[input$Genre]]==1)
+     varlr <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                        input$LengthRange[1], input$LengthRange[2])
+     plot(varlr$length, varlr$rating)
+   })
+   
+   #Analyse des Datensatzes hinsichtloch Durchschnitt, Median etc.
+   output$textInfo <- renderTable({
+     filteredData <- queryfunc(data, input$Genre, input$JahrRange[1], input$JahrRange[2],
+                                input$LengthRange[1], input$LengthRange[2])
+     meanLength <- mean(filteredData[["length"]])
+     medianLength <- median(filteredData[["length"]])
+     length <- c(meanLength, medianLength)
+     
+     meanRating <- mean(filteredData[["rating"]])
+     medianRating <- median(filteredData[["rating"]])
+     rating <- c(meanRating, medianRating)
+     
+     filteredDataBudget <- subset(filteredData, !is.na(filteredData$budget))
+     meanBudget <- mean(filteredDataBudget[["budget"]])
+     medianBudget <- median(filteredDataBudget[["budget"]])
+#      budget <- c(meanBudget, medianBudget)
+
+     analysisTable <-  matrix(c(meanLength, medianLength, 
+                              meanRating, medianRating,
+                              meanBudget,medianBudget),
+                             ncol=2,byrow=TRUE)
+       
+     analysisTable <- as.table(analysisTable)
+#      colnames(analysisTable) <- c("Mean", "Median")
+#      rownames(analysisTable) <- c("Length", "Rating", "Budget")
+     
    })
   
   output$jahre <- renderPrint({input$JahrRange})
   output$genre <- renderPrint({input$Genre})
-  output$test <- renderPrint({str(data)})
 })
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -10,7 +10,7 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
 
-      sliderInput("JahrRange", label = "Range:",
+      sliderInput("JahrRange", label = "Zeitspanne:",
                   min = 1900, max = 2005, value = c(1990,2000)),
       
       selectInput("Genre", label = "Genre:", 
@@ -18,15 +18,27 @@ shinyUI(fluidPage(
                                  "Animation" = "Animation", "Comedy"= "Comedy",
                                  "Drama" = "Drama", "Documentary" = "Documentary", 
                                  "Romance" = "Romance", "Short" = "Short"
-                                   ))
+                                   )),
+      
+      sliderInput("LengthRange", label = "Filmdauer:",
+                  min = 1, max = 300, value = c(80,120))
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("ImdbPlot"),
-      textOutput("genre"),
-      textOutput("jahre"),
-      textOutput("test")
+      tabsetPanel(
+        tabPanel("Jahr/Laenge", plotOutput("ImdbJahrLaenge"), "Durchschnittslaenge
+                 und Median der Laenge sind:"), 
+        tabPanel("Jahr/Rating", plotOutput("ImdbJahrRating")),
+        tabPanel("Jahr/Budget", plotOutput("ImdbJahrBudget")),
+        tabPanel("Budget/Rating", plotOutput("ImdbBudgetRating")),
+        tabPanel("Length/Rating", plotOutput("ImdbLengthRating")),
+        tabPanel("Analyse", textOutput("budget"))
+      ),
+      tags$hr(),
+      tableOutput("textInfo")
+      #textOutput("budget")
+      #textOutput("genre"),
     )
     
   )
